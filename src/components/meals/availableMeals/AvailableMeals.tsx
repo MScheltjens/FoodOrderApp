019c5b35c236458332537c6dabbb406247e96450
Mathias Scheltjens <mathias.scheltjens@icapps.com>
2022-11-04 15:20:20 +0100

    card added

--- src/components/meals/availableMeals/AvailableMeals.tsx
+++ src/components/meals/availableMeals/AvailableMeals.tsx
@@ -1,14 +1,17 @@
 import classes from "./AvailableMeals.module.css";
 import { DUMMY_MEALS as mealsList } from "../../../data/dummyMeals";
+import Card from "../../ui/card/Card";
 
 const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
-      <ul>
-        {mealsList.map(({ id, name }) => (
-          <li key={id}>{name}</li>
-        ))}
-      </ul>
+      <Card>
+        <ul>
+          {mealsList.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </Card>
     </section>
   );
 };
---
+++ src/components/ui/card/Card.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+import classes from "./Card.module.css";
+type CardProps = {
+  children: ReactNode;
+};
+
+const Card = ({ children }: CardProps) => {
+  return <div className={classes.card}>{children}</div>;
+};
+
+export default Card;
